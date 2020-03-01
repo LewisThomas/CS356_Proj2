c902ff4e0206f7d78d3ed51525d7db7bb812a9d7
fecorax <ryan.d.arnesen@gmail.com>
2020-02-29 23:05:53 -0700

    add tags

---
+++ game_chest/src/components/BottomSideBarItem/BottomSideBarItem.js
@@ -0,0 +1,86 @@
+
+import React, { Component } from 'react';
+import {
+    Col, Form,
+    FormGroup, Label, Input,
+    Button,
+} from 'reactstrap';
+import { Redirect } from 'react-router-dom'
+import './BottomSideBarItem.css'
+
+
+class BottomSideBarItem extends Component {
+
+    constructor(props) {
+        super(props);
+
+        //this.switchBool = this.switchBool.bind(this);
+        //this.onChange = this.onChange.bind(this);
+        // let text = this.props.buttonText
+        this.state = {
+            inputing: false,
+            newTagText: "derp"
+        };
+    }
+    setInputing = () => {
+        this.setState({
+            inputing: !this.state.inputing
+        })
+    }
+    setRedirect = () => {
+        this.setState({
+            redirect: true
+        })
+    }
+    renderRedirect = () => {
+        if (this.state.redirect) {
+            return <Redirect to='/main' />
+        }
+    }
+    submitHandler = (event) => {
+        this.setState({
+            newTagText: ""
+        })
+        this.setInputing();
+        event.preventDefault();
+        this.props.onChange(this.state.newTagText);
+    }
+    changeHandler = (event) =>
+    {
+        this.setState({
+            newTagText: event.target.value
+        })
+    }
+
+    // function getBool() {
+    //   console.log("got bool", testBoolean)
+    //   return testBoolean;
+    // }
+    //<Input type="text" name="testInput" id="tagID" placeholder="ABCDEF" value={this.state.textInput} onChange={this.onChange} />
+
+    render() {
+        if (this.state.inputing) {
+            return (
+
+                <div style={{ backgroundColor: this.props.buttonColor, margin: "5px", height: "79px", textAlign: "center", paddingTop: "10px", width: '15.4vw', position: 'fixed', bottom: 5 }} >
+                    <form height="79px" onSubmit={this.submitHandler}>
+                        <input type="text" onChange={this.changeHandler}/>
+                    </form>
+                </div>
+            );
+
+        }
+        else {
+            return (
+
+                <div style={{ backgroundColor: this.props.buttonColor, margin: "5px", height: "79px", textAlign: "center", paddingTop: "10px", width: '15.4vw', position: 'fixed', bottom: 5 }}
+                    onClick={this.setInputing} >
+                    <b className="gameChestText" style={{ margin: "auto", fontSize: "3em" }}>{this.props.buttonText}</b>
+                </div>
+            );
+        }
+
+    }
+}
+
+export default BottomSideBarItem;
--- game_chest/src/pages/main/MainPage.js
+++ game_chest/src/pages/main/MainPage.js
@@ -1,10 +1,11 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 // import {BrowserRouter as Router,
 //         Switch,
 //         Route} from 'react-router-dom'
 // import logo from './logo.svg';
-import {Navbar, Media} from 'reactstrap'
+import { Navbar, Media } from 'reactstrap'
 import SidebarItem from '../../components/SidebarItem/SidebarItem'
+import BottomSidebarItem from '../../components/BottomSideBarItem/BottomSideBarItem'
 import person from './assets/personIcon.svg'
 import PlayerSelector from '../../components/PlayersSelector/PlayersSelector'
 // import './App.css';
@@ -17,12 +18,15 @@ class MainPage extends Component {
   constructor(props) {
     super(props);
 
+    this.getFilterButtons = this.getFilterButtons.bind(this);
     this.switchBool = this.switchBool.bind(this);
     this.onChange = this.onChange.bind(this);
 
     this.state = {
       toggle: false,
-      textInput: ""
+      textInput: "",
+      newTagName: "",
+      sideBarNames: ["Dice", "Strategy"]
     };
   }
 
@@ -31,20 +35,34 @@ class MainPage extends Component {
       toggle: !this.state.toggle
     })
     console.log("switched", this.state.toggle)
-  
+
+  }
+  onTextSubmit = (value) =>
+  {
+    var temp = this.state.sideBarNames;
+    temp.push(value);
+    console.log(value);
+    this.setState({
+      sideBarNames: temp
+    })
+  }
+
+  onTextChange = (event) =>
+  {
+    this.setState({newTagText: event.target.value});
   }
 
   onChange = (event) => {
     console.log(event)
     this.setState({
-      textInput: event.target.value 
+      textInput: event.target.value
     })
   }
 
-  tempSidebarButtonNames = ["Dice", "Strategy"]
+  
   getFilterButtons = () => {
-    return this.tempSidebarButtonNames.map(function(v, i){
-      return <SidebarItem buttonText={v} buttonColor="#6B8CE6"/>
+    return this.state.sideBarNames.map(function (v, i) {
+      return <SidebarItem buttonText={v} buttonColor="#6B8CE6" />
     })
   }
 
@@ -52,32 +70,34 @@ class MainPage extends Component {
   //   console.log("got bool", testBoolean)
   //   return testBoolean;
   // }
-// prim color #4476FF
-// secondary color #6B8CE6
-// 
+  // prim color #4476FF
+  // secondary color #6B8CE6
+  // 
   render() {
     return (
-          <div style={{height:"100%"}}>
-          <Navbar style={{backgroundColor: "#4476FF", margin:"0pt",textAlign: "center"}}>
-            <h1 className="gameChestText">Game Chest</h1>
-          </Navbar>
-          <div style={{width: "100%", display:"table", height:"50.8rem"}}>
-            <div style={{display: ""}}>
-              <div style={{width:"16vw", float:"left", borderStyle:"solid", borderColor: "#707070", borderWidth: "5px",textAlign: "center"}}>
-                <SidebarItem buttonText="Filter By" buttonColor="#4476FF"/>
-                <div style={{backgroundColor:"black", height:"6px"}}/>
-                {this.getFilterButtons()}
-              </div>
-              <div style={{display: "block"}}>
-                <div style={{backgroundColor:"#6B8CE6", marginLeft:"16.5vw", borderColor:"#F5F5F5", borderStyle:"solid", borderWidth:"5px", height:"75px", boxSizing:"border-box"}}>
-                   <b className="gameChestText" style={{float:"left", fontSize:"35pt", marginLeft:"0.2em"}}>Players</b>
-                   <PlayerSelector/>
-                </div>
+      <div style={{ height: "100%" }}>
+        <Navbar style={{ backgroundColor: "#4476FF", margin: "0pt", textAlign: "center" }}>
+          <h1 className="gameChestText">Game Chest</h1>
+        </Navbar>
+        <div style={{ width: "100%", display: "table", height: "50.8rem" }}>
+          <div style={{ display: "" }}>
+            <div style={{ width: "16vw", float: "left", borderStyle: "solid", borderColor: "#707070", borderWidth: "5px", textAlign: "center", height: "780px" }}>
+              <SidebarItem buttonText="Filter By" buttonColor="#4476FF" />
+              <div style={{ backgroundColor: "black", height: "6px" }} />
+              {this.getFilterButtons()}
+              <BottomSidebarItem buttonText="Add Tag" buttonColor="#4476FF" value={this.state.newTagName} onChange={this.onTextSubmit}>
+              </BottomSidebarItem>
+            </div>
+            <div style={{ display: "block" }}>
+              <div style={{ backgroundColor: "#6B8CE6", marginLeft: "16.5vw", borderColor: "#F5F5F5", borderStyle: "solid", borderWidth: "5px", height: "75px", boxSizing: "border-box" }}>
+                <b className="gameChestText" style={{ float: "left", fontSize: "35pt", marginLeft: "0.2em" }}>Players</b>
+                <PlayerSelector />
               </div>
             </div>
           </div>
-          {/* hello */}
-          </div>
+        </div>
+        {/* hello */}
+      </div>
     );
   }
 }
